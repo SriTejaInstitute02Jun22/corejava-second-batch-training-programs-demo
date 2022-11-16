ab2dd92563078054010c377a0f8aeab655d67f3c
ramesh cheppali <ramesh96420@gmail.com>
2022-11-16 21:55:14 +0530

    mapping relation ship between interface to interface

--- src/com/sriteja/oops/CompanyInterface.java
+++ src/com/sriteja/oops/CompanyInterface.java
@@ -1,7 +1,17 @@
 package com.sriteja.oops;
 
+import com.sriteja.bean.Company;
+
 public interface CompanyInterface {
 
+	/***
+	 * get the company address details
+	 */
 	public void getCompanyAddressDetails();
+	
+	/***
+	 * get the company details
+	 */
+	public Company getCompanyDetails();
 	
 }
